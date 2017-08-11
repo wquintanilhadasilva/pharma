606edc9712ee11ae08765a1ca731f826a0edfc39
Wedson Quintanilha da Silva <wquintanilha@gmail.com>
2017-08-11 17:59:55 -0300

    firulas na interface

--- src/app/home/editar-item-pedido/editar-item-pedido.component.ts
+++ src/app/home/editar-item-pedido/editar-item-pedido.component.ts
@@ -19,16 +19,6 @@ import { Order } from './../../domain/Order';
 import { ItemOrder } from './../../domain/ItemOrder';
 import { FaturamentoService } from './../services/faturamento.service';
 
-
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-
 @Component({
   selector: 'app-editar-item-pedido',
   templateUrl: './editar-item-pedido.component.html',
@@ -44,15 +34,10 @@ export class EditarItemPedidoComponent implements OnInit {
   itemOriginal;
   pedido;
   pedidoSimulado;
-
   editado = false;
-
   margemGlobal = 0;
-
   novoItem = false;
-
   exibir = false;
-
   searching = false;
   searchFailed = false;
 
@@ -89,10 +74,7 @@ export class EditarItemPedidoComponent implements OnInit {
   search = (text$: Observable<string>) =>
     _do.call(
       switchMap.call(
-        _do.call(
-          distinctUntilChanged.call(
-            debounceTime.call(text$, 300)),
-          () => this.searching = true),
+        _do.call(distinctUntilChanged.call(debounceTime.call(text$, 300)), () => this.searching = true),
         term =>
           _catch.call(
             _do.call(this.carregaProdutos(term)
@@ -104,7 +86,8 @@ export class EditarItemPedidoComponent implements OnInit {
             }
           )
       ),
-      () => this.searching = false);
+      () => this.searching = false
+  );
 
   constructor(private pedidoService: PedidosService) { }
 
